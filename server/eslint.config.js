9d66f6d3fb176d72bfdcb8e087944fc48baad9b1
nlordduck <223183259+n1lordduck@users.noreply.github.com>
2026-06-13 12:38:03 -0300

    chore(lint): configure eslint with separate node and browser globals

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
-    files: ["src/**/*.js", "public/js/**/*.js"],
+    files: ["src/**/*.js"],
     rules: {
       "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "no-undef": "error",
@@ -14,9 +15,23 @@ export default [
       ecmaVersion: 2022,
       sourceType: "module",
       globals: {
-        console: "readonly",
-        process: "readonly",
-        Buffer:  "readonly",
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["public/js/**/*.js"],
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-undef": "error",
+      "eqeqeq": "error",
+      "no-var": "error",
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
       },
     },
   },
